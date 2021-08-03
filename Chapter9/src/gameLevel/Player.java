33e2b8bcc6d551a921f9d06f7e32ced8845b74ce
Hanjeongwoo <hotgkswjddn@naver.com>
2021-08-03 23:25:17 +0900

    init commit

---
+++ Chapter9/src/gameLevel/Player.java
@@ -0,0 +1,23 @@
+package gameLevel;
+
+public class Player {
+	private PlayerLevel level; //Player가 가지는 level 변수 선언
+	
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
+	
+	public PlayerLevel getLevel() {
+		return level;
+	}
+	
+	public void upgradeLevel(PlayerLevel level) {
+		this.level = level;
+		level.showLevelMessage();
+	}
+	
+	public void play(int count) {
+		level.go(count);
+	}
+}
